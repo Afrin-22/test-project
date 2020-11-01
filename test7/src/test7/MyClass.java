8aaf90418c5ae6b19c8534f47cebf4fb3fcc5444
Afrin-22 <afrin.shaza22@gmail.com>
2020-11-01 20:34:54 +0530

    Initial commit.

---
+++ test7/src/test7/MyClass.java
@@ -0,0 +1,32 @@
+package test7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of array : ");
+		int l = sc.nextInt();
+		System.out.println("Enter inputs : ");
+		int[] arr = new int[l];
+		for(int i=0; i<l; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		int element=0;
+		for(int i=0; i<l; i++) {
+			for(int j=i+1; j<l; j++) {
+				if(arr[i]<arr[j]) {
+					element=arr[i];
+				}
+			}
+		}		
+		System.out.println("Smallest element in array is "+element);
+	}
+
+}
